c7e81e604d401877924fbc2bdc235e51161702d7
Sahana <sahanag2809@gmail.com>
2024-11-28 15:25:18 +0530

    updations

---
+++ index.js
@@ -0,0 +1,16 @@
+function loadPage(page) {
+    fetch(page)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Page not found");
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById("content").innerHTML = data;
+        })
+        .catch(error => {
+            console.error(error);
+            document.getElementById("content").innerHTML = "<h1>404 - Page Not Found</h1>";
+        });
+}
